6a0cea519b507060114259d3c3be7cb6be4aa595
desktopadsdk <icsofticsoft@yanzhi.local>
2015-07-23 16:08:01 +0800

    version 1.5

--- SDK/DesktopAD.h
+++ SDK/DesktopAD.h
@@ -5,7 +5,7 @@
 //  Created by Coding on 14-12-8.
 //  Copyright (c) 2014年 DesktopAD. All rights reserved.
 
-//  SDK Version 1.4
+//  SDK Version 1.5
 
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
